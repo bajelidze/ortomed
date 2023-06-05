5a5f5855faba34792eec1ec182f2760bfe4086a9
bajelidze <irakli.bajelidze@exasol.com>
2023-06-05 19:48:29 +0200

    Set identation to `2`; Add `Vuetify`

--- electron/main.ts
+++ electron/main.ts
@@ -19,28 +19,28 @@ let win: BrowserWindow | null
 const VITE_DEV_SERVER_URL = process.env['VITE_DEV_SERVER_URL']
 
 function createWindow() {
-    win = new BrowserWindow({
-        icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-        },
-    })
+  win = new BrowserWindow({
+    icon: path.join(process.env.PUBLIC, 'electron-vite.svg'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
 
-    // Test active push message to Renderer-process.
-    win.webContents.on('did-finish-load', () => {
-        win?.webContents.send('main-process-message', (new Date).toLocaleString())
-    })
+  // Test active push message to Renderer-process.
+  win.webContents.on('did-finish-load', () => {
+    win?.webContents.send('main-process-message', (new Date).toLocaleString())
+  })
    
-    if (VITE_DEV_SERVER_URL) {
-        win.loadURL(VITE_DEV_SERVER_URL)
-    } else {
-        // win.loadFile('dist/index.html')
-        win.loadFile(path.join(process.env.DIST, 'index.html'))
-    }
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL)
+  } else {
+    // win.loadFile('dist/index.html')
+    win.loadFile(path.join(process.env.DIST, 'index.html'))
+  }
 }
 
 app.on('window-all-closed', () => {
-    win = null
+  win = null
 })
 
 app.whenReady().then(createWindow)
