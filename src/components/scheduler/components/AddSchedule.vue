a4e720ce16777a503db0167890974438d900faf2
bajelidze <irakli.bajelidze@exasol.com>
2024-02-18 16:38:44 +0100

    Add autocomplete fields for Patient, Doctor, Course

--- src/components/scheduler/components/AddSchedule.vue
+++ src/components/scheduler/components/AddSchedule.vue
@@ -6,8 +6,70 @@
   >
     <v-container>
       <v-row>
-        <v-col>
-          text
+        <v-col cols="5">
+          <CardItem
+            title="Patient"
+            icon="mdi-account"
+          >
+            <v-row>
+              <v-col cols="1"/>
+              <v-col cols="6">
+                <v-autocomplete
+                  v-model="patient"
+                  density="compact"
+                  label="Patient*"
+                  :disabled="submitLoading"
+                  :items="patientsNames"
+                />
+              </v-col>
+            </v-row>
+          </CardItem>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="5">
+          <CardItem
+            title="Doctor"
+            icon="mdi-medical-bag"
+            class="mt-4"
+          >
+            <v-row>
+              <v-col cols="1"/>
+              <v-col cols="6">
+                <v-autocomplete
+                  v-model="doctor"
+                  density="compact"
+                  label="Doctor*"
+                  :disabled="submitLoading"
+                  :items="doctorNames"
+                />
+              </v-col>
+            </v-row>
+          </CardItem>
+        </v-col>
+      </v-row>
+
+      <v-row>
+        <v-col cols="5">
+          <CardItem
+            title="Course"
+            icon="mdi-list-box"
+            class="mt-4"
+          >
+            <v-row>
+              <v-col cols="1"/>
+              <v-col cols="6">
+                <v-autocomplete
+                  v-model="course"
+                  density="compact"
+                  label="Course*"
+                  :disabled="submitLoading"
+                  :items="courseNames"
+                />
+              </v-col>
+            </v-row>
+          </CardItem>
         </v-col>
       </v-row>
     </v-container>
@@ -25,11 +87,26 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import MsgSnackbar from '../../common/MsgSnackbar.vue';
+import CardItem from '../../common/CardItem.vue';
 import { SubmitFormProps } from '../../../common/props';
 import { Scheduler } from '../../../common/events';
 
+const patients = await window.api.patients.listAll();
+const patientsNames = patients.map(patient => patient.name + ' (' + patient.id + ')');
+
+const doctors = await window.api.doctors.listAll();
+const doctorNames = doctors.map(doctor => doctor.name + ' (' + doctor.id + ')');
+
+const courses = await window.api.courses.listAll();
+const courseNames = courses.map(course => course.name + ' (' + course.id + ')');
+
+// Contains: "$name ($id)"
+const patient = ref('');
+const doctor = ref('');
+const course = ref('');
+
 const emit = defineEmits<{
-  (e: typeof Scheduler.ADD_SCHEDULE_SUBMIT, fields: any): void;
+ (e: typeof Scheduler.ADD_SCHEDULE_SUBMIT, fields: any): void;
 }>();
 
 defineProps<SubmitFormProps>();
